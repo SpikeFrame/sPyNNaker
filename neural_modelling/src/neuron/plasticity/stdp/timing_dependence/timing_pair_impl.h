839a8294d4684107ee7fcb1fa6d3dfbfb23bdbfc
EricNichols <e.nichols@surrey.ac.uk>
2016-05-17 15:54:52 +0100

    Minor change to timing dependence.

--- neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_pair_impl.h
+++ neural_modelling/src/neuron/plasticity/stdp/timing_dependence/timing_pair_impl.h
@@ -1,44 +1,15 @@
-/*
- * The supervised learning rule is implemented in this file.
- * s = Tar-Pre or Out-Pre at times of Tar and Out
- * PostSynaptic Potential (PSP) at s = exp**(-s/tauMem)
- * where tauMem and tauSyn are time constants of neuron and synapse.
- * At learning pattern end,
- * all the PSP from Tar are summed and subtracted from all summed Out PSP.
- *
- * To implement the rule in this file...
- * we have timing_apply_post_spike(...) parameters
- * time = Tar or Out
- * last_pre_time = Pre
- * post_trace_t = action potential, denotes if Tar (=0) or Out (=1)
- * time_since_last_pre = s
- * DECAY_LOOKUP_TAU_PLUS( time_since_last_pre) = exp**(-s/tauMem)
- * previous_state.accumLast   = current update
- * previous_state.accumulator = accumulated prior updates
- * previous_state.weight_state = weight change at end of learning pattern
- *
- */
-
-#ifndef _TIMING_TARGET_PAIR_IMPL_H_
-#define _TIMING_TARGET_PAIR_IMPL_H_
+#ifndef _TIMING_PAIR_IMPL_H_
+#define _TIMING_PAIR_IMPL_H_
 
 //---------------------------------------
 // Typedefines
 //---------------------------------------
 typedef int16_t post_trace_t;
-static post_trace_t timing_get_initial_post_trace();
-address_t timing_initialise(address_t address);
-static update_state_t timing_apply_post_spike(uint32_t time,
-		post_trace_t syn_signal, uint32_t last_pre_time,
-		update_state_t previous_state);
+typedef int16_t pre_trace_t;
 
-//---------------------------------------
-// Includes
-//---------------------------------------
-#include "../synapse_structure/synapse_structure_weight_target.h"
-#include "../synapse_structure/synapse_structure.h"
+#include "../synapse_structure/synapse_structure_weight_impl.h"
+#include "timing.h"
 #include "../weight_dependence/weight_one_term.h"
-//#include "timing.h"
 
 // Include debug header for log_info etc
 #include <debug.h>
@@ -54,15 +25,22 @@ static update_state_t timing_apply_post_spike(uint32_t time,
 #define TAU_PLUS_TIME_SHIFT 0
 #define TAU_PLUS_SIZE 256
 
+#define TAU_MINUS_TIME_SHIFT 0
+#define TAU_MINUS_SIZE 256
+
 // Helper macros for looking up decays
 #define DECAY_LOOKUP_TAU_PLUS(time) \
     maths_lut_exponential_decay( \
         time, TAU_PLUS_TIME_SHIFT, TAU_PLUS_SIZE, tau_plus_lookup)
+#define DECAY_LOOKUP_TAU_MINUS(time) \
+    maths_lut_exponential_decay( \
+        time, TAU_MINUS_TIME_SHIFT, TAU_MINUS_SIZE, tau_minus_lookup)
 
 //---------------------------------------
 // Externals
 //---------------------------------------
 extern int16_t tau_plus_lookup[TAU_PLUS_SIZE];
+extern int16_t tau_minus_lookup[TAU_MINUS_SIZE];
 
 //---------------------------------------
 // Timing dependence inline functions
@@ -71,85 +49,97 @@ static inline post_trace_t timing_get_initial_post_trace() {
     return 0;
 }
 
-// The synaptic signals (syn_signal) that can be sent to post-events:
-//   0: Learning is ongoing, accumulate values
-// 1-3: A spike passed through a target synapse and
-//      1: Turning on Learning
-//      2: Learning is ongoing, accumulate values
-//      3: The current pattern has come to an end, update weight
+//---------------------------------------
+static inline post_trace_t timing_add_post_spike(
+        uint32_t time, uint32_t last_time, post_trace_t last_trace) {
 
+    // Get time since last spike
+    uint32_t delta_time = time - last_time;
+
+    // Decay previous o1 and o2 traces
+    int32_t decayed_o1_trace = STDP_FIXED_MUL_16X16(last_trace,
+            DECAY_LOOKUP_TAU_MINUS(delta_time));
+
+    // Add energy caused by new spike to trace
+    // **NOTE** o2 trace is pre-multiplied by a3_plus
+    int32_t new_o1_trace = decayed_o1_trace + STDP_FIXED_POINT_ONE;
+
+    log_debug("\tdelta_time=%d, o1=%d\n", delta_time, new_o1_trace);
+
+    // Return new pre- synaptic event with decayed trace values with energy
+    // for new spike added
+    return (post_trace_t) new_o1_trace;
+}
 
 //---------------------------------------
-// This will apply an actual postsynaptic spike
-// usefull variables:
-// time          = postsynaptic (+ dendritic delay) or target spike time
-// last_pre_time = last presynaptic spike time
+static inline pre_trace_t timing_add_pre_spike(
+        uint32_t time, uint32_t last_time, pre_trace_t last_trace) {
+
+    // Get time since last spike
+    uint32_t delta_time = time - last_time;
+
+    // Decay previous r1 and r2 traces
+    int32_t decayed_r1_trace = STDP_FIXED_MUL_16X16(
+        last_trace, DECAY_LOOKUP_TAU_PLUS(delta_time));
+
+    // Add energy caused by new spike to trace
+    int32_t new_r1_trace = decayed_r1_trace + STDP_FIXED_POINT_ONE;
+
+    log_debug("\tdelta_time=%u, r1=%d\n", delta_time, new_r1_trace);
+
+    // Return new pre-synaptic event with decayed trace values with energy
+    // for new spike added
+    return (pre_trace_t) new_r1_trace;
+}
+
+//---------------------------------------
+static inline update_state_t timing_apply_pre_spike(
+        uint32_t time, pre_trace_t trace, uint32_t last_pre_time,
+        pre_trace_t last_pre_trace, uint32_t last_post_time,
+        post_trace_t last_post_trace, update_state_t previous_state) {
+    use(&trace);
+    use(last_pre_time);
+    use(&last_pre_trace);
+
+    // Get time of event relative to last post-synaptic event
+    uint32_t time_since_last_post = time - last_post_time;
+    if (time_since_last_post > 0) {
+        int32_t decayed_o1 = STDP_FIXED_MUL_16X16(
+            last_post_trace, DECAY_LOOKUP_TAU_MINUS(time_since_last_post));
+
+        log_debug("\t\t\ttime_since_last_post_event=%u, decayed_o1=%d\n",
+                  time_since_last_post, decayed_o1);
+
+        // Apply depression to state (which is a weight_state)
+        return weight_one_term_apply_depression(previous_state, decayed_o1);
+    } else {
+        return previous_state;
+    }
+}
+
+//---------------------------------------
 static inline update_state_t timing_apply_post_spike(
-        uint32_t time, post_trace_t syn_signal, uint32_t last_pre_time,
-		update_state_t previous_state) {
+        uint32_t time, post_trace_t trace, uint32_t last_pre_time,
+        pre_trace_t last_pre_trace, uint32_t last_post_time,
+        post_trace_t last_post_trace, update_state_t previous_state) {
+    use(&trace);
+    use(last_post_time);
+    use(&last_post_trace);
 
     // Get time of event relative to last pre-synaptic event
     uint32_t time_since_last_pre = time - last_pre_time;
+    if (time_since_last_pre > 0) {
+        int32_t decayed_r1 = STDP_FIXED_MUL_16X16(
+            last_pre_trace, DECAY_LOOKUP_TAU_PLUS(time_since_last_pre));
 
-    // a spike came from a non-Target neuron
-	if (syn_signal==0)
-	{
-	    if (time_since_last_pre > 0) // within learning pattern time frame
-	    {
-			// io_printf(IO_BUF,"time_since_last_pre: %dms\n", time_since_last_pre);
-			log_debug("\t\t\ttime_since_last_pre_event=%u\n",time_since_last_pre);
+        log_debug("\t\t\ttime_since_last_pre_event=%u, decayed_r1=%d\n",
+                  time_since_last_pre, decayed_r1);
 
-			// add last synaptic update to accumulation
-			previous_state.accumulator += previous_state.accumLast;
-
-			// update the last accumulation
-			previous_state.accumLast = -1 * DECAY_LOOKUP_TAU_PLUS( time_since_last_pre);
-	    }
-	}
-
-	// a doublet passed through a Target synapse, starting learning
-	if (syn_signal==1)
-	{
-		previous_state.accumulator = 0; // reset accumulator to baseline
-		previous_state.accumLast   = 0; // reset accumLast to baseline
-	}
-
-	// Learning is on and a spike passed through a Target synapse
-	else if (syn_signal==2)
-	{
-		if (time_since_last_pre > 0) // within learning pattern time frame
-		{
-			// add last synaptic update to accumulation
-			previous_state.accumulator += previous_state.accumLast;
-
-			// update the last accumulation
-			previous_state.accumLast = DECAY_LOOKUP_TAU_PLUS( time_since_last_pre);
-		}
-	}
-
-	// Learning is on and a doublet passed through a Target synapse
-	else if (syn_signal==3)
-	{
-		// Apply potentiation to state (which is a weight_state) if positive
-		if (previous_state.accumulator > 0)
-		{
-			previous_state.weight_state =
-					weight_one_term_apply_potentiation(
-							previous_state.weight_state,
-							previous_state.accumulator);
-		}
-
-		// Apply depression to state (which is a weight_state) if negative
-		else if (previous_state.accumulator < 0)
-		{
-			previous_state.weight_state =
-					weight_one_term_apply_depression(
-							previous_state.weight_state,
-							(-1 * previous_state.accumulator));
-		}
-	}
-
-    return previous_state;
+        // Apply potentiation to state (which is a weight_state)
+        return weight_one_term_apply_potentiation(previous_state, decayed_r1);
+    } else {
+        return previous_state;
+    }
 }
 
-#endif // _TIMING_TARGET_PAIR_IMPL_H_
+#endif // _TIMING_PAIR_IMPL_H_
