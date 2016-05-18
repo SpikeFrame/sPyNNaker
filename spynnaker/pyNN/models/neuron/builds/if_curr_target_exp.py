4d6d47b51b4fb96c10c884fba74dd2f3150138a6
EricNichols <e.nichols@surrey.ac.uk>
2016-05-18 10:42:16 +0100

    Debug update.

--- spynnaker/pyNN/models/neuron/builds/if_curr_target_exp.py
+++ spynnaker/pyNN/models/neuron/builds/if_curr_target_exp.py
@@ -2,7 +2,7 @@ from spynnaker.pyNN.models.neuron.neuron_models\
     .neuron_model_leaky_integrate_and_fire \
     import NeuronModelLeakyIntegrateAndFire
 from spynnaker.pyNN.models.neuron.synapse_types.synapse_type_exponential_target \
-    import SynapseTypeExponential_target
+    import SynapseTypeExponentialTarget
 from spynnaker.pyNN.models.neuron.input_types.input_type_current \
     import InputTypeCurrent
 from spynnaker.pyNN.models.neuron.threshold_types.threshold_type_static \
